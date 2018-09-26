6fc0b804c8dbfcd60330f4cf17881f5cfe0f8baf
Corado M. Burcus <corado.burcus@osedea.com>
2018-09-26 16:06:32 -0400

    Initial commit

---
+++ test/example.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('TESTING: example', () => {
+  it('it has an example test', async () => {
+    expect(true).to.equal(true);
+  });
+});
